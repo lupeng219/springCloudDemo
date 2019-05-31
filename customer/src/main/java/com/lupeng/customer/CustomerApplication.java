11fa86341367dacef30106757ce6250e0bee2c98
lupeng <1055663741@qq.com>
2019-05-31 16:43:48 +0800

    config 注册到eureka

--- customer/src/main/java/com/lupeng/customer/CustomerApplication.java
+++ customer/src/main/java/com/lupeng/customer/CustomerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.lupeng.customer.serviceFeign")
 public class CustomerApplication {
 
--- customer/src/main/java/com/lupeng/customer/HelloControler.java
+++ customer/src/main/java/com/lupeng/customer/HelloControler.java
@@ -5,11 +5,14 @@ import com.lupeng.customer.service.HelloService;
 import com.lupeng.customer.serviceFeign.HelloServiceFeign;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloControler {
 
@@ -23,6 +26,13 @@ public class HelloControler {
 //    {
 //        return helloService.hiService(name);
 //    }
+    @Value("${hello}")
+    private String hello;
+
+    @RequestMapping("hello")
+    public String hello() {
+        return hello;
+    }
 
     @RequestMapping(value = "/hii")
     @HystrixCommand(fallbackMethod = "hiError")
