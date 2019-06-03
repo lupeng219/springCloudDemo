9543702cbcf0db55fa7e2fcd2475d22a68230dfb
lupeng <1055663741@qq.com>
2019-06-03 11:27:27 +0800

    no message

--- producer/src/main/java/com/lupeng/producer/ProducerApplication.java
+++ producer/src/main/java/com/lupeng/producer/ProducerApplication.java
@@ -3,6 +3,7 @@ package com.lupeng.producer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@RefreshScope
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
