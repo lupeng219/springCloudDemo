c9161535fe6bb1ba6b164ee53454382c720a1fd4
lupeng <1055663741@qq.com>
2019-06-25 16:20:57 +0800

    .

--- customer/src/main/java/com/lupeng/customer/mq/ConsumeClient.java
+++ customer/src/main/java/com/lupeng/customer/mq/ConsumeClient.java
@@ -2,8 +2,7 @@ package com.lupeng.customer.mq;
 
 public class ConsumeClient {
     public static void main(String[] args) throws Exception{
-        MqClient mqClient = new MqClient();
-        String message = mqClient.consume();
+        String message = MqClient.consume();
         System.out.println("获取的消息为：" + message);
     }
 }
--- customer/src/main/java/com/lupeng/customer/mq/ProduceClient.java
+++ customer/src/main/java/com/lupeng/customer/mq/ProduceClient.java
@@ -2,7 +2,6 @@ package com.lupeng.customer.mq;
 
 public class ProduceClient {
     public static void main(String[] args) throws Exception {
-        MqClient client = new MqClient();
-        client.produce("SEND:Hello Worlds");
+        MqClient.produce("SEND:Hello Worlds");
     }
 }
